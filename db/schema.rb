43f296461fd3d07ceebfa0d4ea10d323d6565817
Tommy Nieuwenhuis <thomas.nieuwenhuis24@gmail.com>
2021-05-13 11:19:42 -0600

    Add validation testing to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_13_165909) do
+ActiveRecord::Schema.define(version: 2021_05_13_171147) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_05_13_165909) do
     t.string "zipcode"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "username"
   end
 
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe "validations" do
+    it {should validate_presence_of :username}
+    it {should validate_uniqueness_of :username}
+    it {should validate_presence_of :first_name}
+    it {should validate_presence_of :last_name}
+    it {should validate_presence_of :address}
+    it {should validate_presence_of :city}
+    it {should validate_presence_of :zipcode}
+  end
+end
