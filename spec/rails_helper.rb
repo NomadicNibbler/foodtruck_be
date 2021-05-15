f8039dad3f48a47d11e604cf6d989ccbe0c21bb7
Wil McCauley <wmccauley724@gmail.com>
2021-05-15 13:13:24 -0600

    Add map facade and test file, add webmock and vcr with config blocks in helper files

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -74,3 +74,9 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
+
+VCR.configure do |config|
+  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
+  config.hook_into :webmock
+  config.filter_sensitive_data('<COORD_KEY>') { ENV['COORD_KEY'] }
+end
