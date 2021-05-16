3fe42a1ce12172494db396faf3c5dd3abe843c3c
Wil McCauley <wmccauley724@gmail.com>
2021-05-16 11:25:27 -0600

    Add add distance class method and add distance to attr readers

--- app/poros/region.rb
+++ app/poros/region.rb
@@ -2,12 +2,18 @@ class Region
   attr_reader :name,
               :long,
               :lat,
-              :country
+              :country,
+              :distance
+
   def initialize(data)
     @name = data[:name]
     @lat = data[:latitude]
     @long = data[:longitude]
     @country = data[:country]
     @distance = 0
+  end
+
+  def add_distance(distance)
+    @distance = distance
   end
 end
