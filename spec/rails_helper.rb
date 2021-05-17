02e966b3f204e15416395ec9cb6893cc6feeca2f
kylejschulz <kylschulz@gmail.com>
2021-05-17 10:44:02 -0400

    filter google maps api key from VCR casettes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -79,6 +79,7 @@ VCR.configure do |config|
   config.cassette_library_dir = "#{::Rails.root}/spec/fixtures/vcr_cassettes"
   config.hook_into :webmock
   config.filter_sensitive_data('<COORD_KEY>') { ENV['COORD_KEY'] }
+  config.filter_sensitive_data('<GOOGLE_MAPS_API_KEY>') { ENV['GOOGLE_MAPS_API_KEY'] }
   config.allow_http_connections_when_no_cassette = true
   config.configure_rspec_metadata!
   config.ignore_localhost = true
