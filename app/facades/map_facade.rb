549e40cca43b12ff3986f199d7fa2d6ff9a51adc
Wil McCauley <wmccauley724@gmail.com>
2021-05-18 16:00:50 -0600

    Remove commented lines

--- app/facades/map_facade.rb
+++ app/facades/map_facade.rb
@@ -1,6 +1,5 @@
 class MapFacade
   def self.get_trucks(address) # + radius
-    # valid_address = valid_address?(address)
     lat_long = address_to_lat_long(address)
     formatted_lat_long = "#{lat_long[:lat]},#{lat_long[:lng]}"
     region = find_closest_region(formatted_lat_long)
@@ -17,11 +16,9 @@ class MapFacade
   def self.find_closest_region(user_location)
     regions_with_distance = regions.each do |region|
       region_loc = "#{region.lat},#{region.long}"
-      # user_loc   = "#{user_location[:lat]},#{user_location[:lng]}"
       distance = DistanceService.get_distance(region_loc, user_location)
       region.add_distance(distance)
     end
-    # calling regions method again loses distance data, now stored as regions_with_distance
     ordered_regions = regions_with_distance.sort_by do |region|
       region.distance
     end
@@ -48,7 +45,6 @@ class MapFacade
         truck.add_distance(1000)
       else
         truck_loc = "#{truck.lat},#{truck.long}"
-        # user_loc   = "#{user_location[:lat]},#{user_location[:lng]}"
         distance = DistanceService.get_distance(truck_loc, user_location)
         truck.add_distance(distance)
       end
