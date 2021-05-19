348621977045126aea492f24f57345209a5da290
Wil McCauley <wmccauley724@gmail.com>
2021-05-19 15:35:51 -0600

    Update JSON contract with team

--- json_contract.rb
+++ json_contract.rb
@@ -14,25 +14,7 @@ params: { "first": "name", "last": "name" }
       "first_name": "Beep",
       "last_name": "Beeperson",
       "lat": "45.84858",
-      "long": "69.37472",
-      "trucks": [
-        {
-          "id": "23",
-          "lat": "53.23657",
-          "long": "24.36676",
-          "name": "truck_1",
-          "distance": "2.4",
-          "logo_small": "logo"
-        },
-        {
-          "id": "2",
-          "lat": "67.23657",
-          "long": "28.36676",
-          "name": "truck_2",
-          "distance": "1.2",
-          "logo_small": "logo"
-        }
-      ]
+      "long": "69.37472"
     }
   }
 }
@@ -40,7 +22,7 @@ params: { "first": "name", "last": "name" }
 # new user registration
 
 POST 'api/v1/users'
-params { "first": "Beep", "last": "Beeperson", "street": "22 Hillcrest Dr", "city": "lander", "zip": "82520" }
+params: { "username": "some_beep", "first_name": "Beep", "last_name": "Beeperson", "street": "22 Hillcrest Dr", "city": "lander", "zip": "82520" }
 
 {
   "data": {
@@ -51,24 +33,6 @@ params { "first": "Beep", "last": "Beeperson", "street": "22 Hillcrest Dr", "cit
       "last_name": "Beeperson",
       "lat": "45.84858",
       "long": "69.37472",
-      "trucks": [
-        {
-          "id": "23",
-          "lat": "53.23657",
-          "long": "24.36676",
-          "name": "truck_1",
-          "distance": "2.4",
-          "logo_small": "logo"
-        },
-        {
-          "id": "2",
-          "lat": "67.23657",
-          "long": "28.36676",
-          "name": "truck_2",
-          "distance": "1.2",
-          "logo_small": "logo"
-        }
-      ]
     }
   }
 }
@@ -76,21 +40,40 @@ params { "first": "Beep", "last": "Beeperson", "street": "22 Hillcrest Dr", "cit
 # truck show view
 
 GET 'api/v1/trucks'
-params: { "id": 2 }
 
 {
   "data": {
     "type": "trucks",
-    "id": "2",
+    "id": null,
     "attributes": {
-      "name": "dope foods",
-      "payment_methods": [],
-      "website": "www.dope-af.com",
-      # hashes for each site with logo(s)?
-      "socials": [],
-      "description": "text",
-      "logo": "logo",
-      "display": "street, etc."
+      "trucks": [
+        {
+          "description": "text about truck",
+          "display": "Howe St & W Cordova St",
+          "distance": "6.3",
+          "logo": "some_logo.png",
+          "name": "dope fooodz",
+          "payment_methods": ["cash", "apple_pay", "credit_card"],
+          "phone": "6103592275",
+          "socials": [{"facebook": "handle"},{"twitter": "foodzzz"}],
+          "website": "dope_provisions.eat",
+          "lat": "32.23657",
+          "long": "17.36676"
+        },
+        {
+          "description": "text about truck",
+          "display": "Wynkoop St & Wazee St",
+          "distance": "2.4",
+          "logo": "some_other_logo.png",
+          "name": "secret kebabs",
+          "payment_methods": ["cash", "apple_pay", "debit_card"],
+          "phone": "6102202222",
+          "socials": [{"twitter": "meatz3"}],
+          "website": "secret-meats.org/eat",
+          "lat": "46.23657",
+          "long": "20.36676"
+        }
+      ]
     }
   }
 }
