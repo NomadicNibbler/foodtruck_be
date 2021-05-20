51cd1faf7bb13e948e7cbfe14a23e1fc1d44b4a1
kylejschulz <kylschulz@gmail.com>
2021-05-20 19:10:37 -0400

    remove wait from users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class Api::V1::UsersController < ApplicationController
     user = User.new(user_params)
     if user.save
       render json: UserSerializer.new(user), status: 201
-      wait 1
     else
       render json: {data: { error: "Please make sure all fields are filled in before registering. If error persists after filling out fields, username already exists."}}, status: 400
     end
