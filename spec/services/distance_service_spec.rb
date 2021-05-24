8a21d4bae92086ae932e4df9c61ee653ce68c875
Wil McCauley <wmccauley724@gmail.com>
2021-05-24 17:18:40 -0600

    Fix tests to match refactors

--- spec/services/distance_service_spec.rb
+++ spec/services/distance_service_spec.rb
@@ -2,21 +2,21 @@ require 'rails_helper'
 
 RSpec.describe 'distance service' do
   describe 'class methods' do
-    xit '#get_distance', :vcr do
+    it '#get_distance', :vcr do
       origin = "40.6655101,-73.89188969999998"
       destination = "40.6905615,-73.9976592"
       response = DistanceService.get_distance(origin, destination)
-      expect(response).to be_a(Float)
-      expect(response).to eq(6.5)
+
+      expect(response.keys).to eq([:destination_addresses, :origin_addresses, :rows, :status])
     end
   end
   describe 'sad path' do
-    xit "returns 'NOT_FOUND' for invalid destination" do
+    it "returns 'NOT_FOUND' for invalid destination" do
       origin = "40.6655101,-73.89188969999998"
       destination = "40.6905615,-100000000"
       response = DistanceService.get_distance(origin, destination)
 
-      expect(response).to eq(100000)
+      expect(response[:rows].first[:elements].first[:status]).to eq('NOT_FOUND')
     end
   end
 end
