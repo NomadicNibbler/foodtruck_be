a507564c5a72bc109d7b7db642b8e0f1c2e2fa6b
Wil McCauley <wmccauley724@gmail.com>
2021-05-24 17:23:49 -0600

    Switch out Faker beers to avoid apostrophe errors

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   FactoryBot.define do
     factory :user do
-      username { Faker::Beer.name }
+      username { Faker::JapaneseMedia::DragonBall.character }
       first_name { Faker::Name.first_name }
       last_name { Faker::Name.last_name }
       address { Faker::Address.street_address }
