3a13b229378f9dd9d63fcfa2a85a1a5154e7ff9c
Wil McCauley <wmccauley724@gmail.com>
2021-05-24 16:32:35 -0600

    Fill out attribute testing and unskip add distance

--- spec/poros/truck_spec.rb
+++ spec/poros/truck_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Truck' do
   describe 'it can be instantiated' do
-    xit 'can have attributes', :vcr do
+    it 'can have attributes', :vcr do
       raw = FoodTruckService.get_schedules_by_city('vancouver')
       truck = Truck.new(raw.first[1])
 
@@ -12,12 +12,25 @@ RSpec.describe 'Truck' do
       expect(truck.long).to be_a(Float)
       expect(truck.long).to eq(-123.113481)
       expect(truck.name).to eq("arturosmexico2go")
-      expect(truck.logo_small).to eq("https://cdn.streetfoodapp.com/images/arturos-to-go/logo/1.90w.png")
+      expect(truck.logo).to eq("https://cdn.streetfoodapp.com/images/arturos-to-go/logo/1.90w@2x.png")
+      expect(truck.distance).to eq(0)
+      expect(truck.payment_methods).to eq(["cash", "credit_card", "debit_card", "apple_pay"])
+      expect(truck.website).to eq('')
+      expect(truck.socials).to eq({:facebook => "arturosmexico2go",
+                                   :instagram => "arturos2go",
+                                   :twitter => "arturomexico2go"
+                                   })
+      expect(truck.phone).to eq("(604) 202-4043")
+      # expect(truck.description).to eq("Arturo's unique recipes are a fusion of Spanish and traditional Mexican. Clean, simple and healthy M...ing take out food at open spaces since 2010, and we will continue doing it, safety is our priority.")
+      expect(truck.description_short).to eq("Authentic, author gourmet Mexican made with the finest ingredients hand picked.")
+      expect(truck.display).to eq('')
+      expect(truck.id).to eq(nil)
+      expect(truck.recent_data).to eq(true)
     end
   end
 
   describe "instance methods" do
-    xit "#add_distance" do
+    it "#add_distance" do
       data = {
               id: 1,
               lat: 1234567,
