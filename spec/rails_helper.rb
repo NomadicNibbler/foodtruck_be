d988c7b2840d40febb220134e62e053d0db5b21b
kylejschulz <kylschulz@gmail.com>
2021-05-24 17:49:58 -0400

    update Gemfile.lock

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -45,7 +45,10 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  # flushing
+  # config.before(:each) do
+    # data is not saved into real Redis but you still need to clear it
+    # Redis.flushdb
+  # end
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
