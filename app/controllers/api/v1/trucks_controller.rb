a54c5f0b46ac09ffebfa0aed0e489c8e5ffabbf2
Wil McCauley <wmccauley724@gmail.com>
2021-05-24 14:41:06 -0600

    Fix tests and add guard clauses to handle bad returns from API

--- app/controllers/api/v1/trucks_controller.rb
+++ app/controllers/api/v1/trucks_controller.rb
@@ -3,8 +3,12 @@ class Api::V1::TrucksController < ApplicationController
     @user = User.find(user_params[:id])
     if @user
       trucks = MapFacade.get_trucks("#{@user.address}, #{@user.city}, #{@user.zipcode}")
-      serialized_data = TruckSerializer.new(trucks)
-      render json: serialized_data, status: 200
+      if trucks == "location not found"
+        render json: { data: { error: "Location invalid, please enter another location" }}
+      else
+        serialized_data = TruckSerializer.new(trucks)
+        render json: serialized_data, status: 200
+      end
     else
       render json: {data: { error: "Please enter a valid username."}}, status: 400
     end
--- spec/services/map_service_spec.rb
+++ spec/services/map_service_spec.rb
@@ -14,13 +14,26 @@ RSpec.describe 'the map service' do
   end
 
   describe 'sad path' do
-    it '#get_coords returns 404 for bad address', :vcr do
+    it '#get_coords - api corrects for minor errors when mostly correct', :vcr do
       address = '898 W Breadway Vancouver V5Z 1J8'
       result = MapService.get_coords(address)
-      expect(res).to be_a(Hash)
-      expect(res.keys).to eq([:lat, :lng])
-      expect(res[:lat]).to be_a(Float)
-      expect(res[:lng]).to be_a(Float)
+      expect(result).to be_a(Hash)
+      expect(result.keys).to eq([:lat, :lng])
+      expect(result[:lat]).to be_a(Float)
+      expect(result[:lng]).to be_a(Float)
+    end
+    it '#get_coords returns error message for empty address', :vcr do
+      address = ''
+      result = MapService.get_coords(address)
+
+      expect(result).to be_a(String)
+      expect(result).to eq("location not found")
+    end
+    it '#get_coords returns error message for nonsense address', :vcr do
+      address = 'aldsfkjhlkj34h56lk3j4hdjfshg1232skjdfgh'
+      result = MapService.get_coords(address)
+      expect(result).to be_a(String)
+      expect(result).to eq("location not found")
     end
   end
 end
